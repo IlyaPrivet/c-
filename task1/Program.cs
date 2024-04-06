8ac03840bee2238e9553b30a8c562b9f24a9d62a
Ilya <lobasov1989@ya.ru>
2024-04-06 10:23:31 +0300

    task3 done

---
+++ task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+int RecursiveNumber(int start, int finish)
+{
+    Console.Write($"{start} ");
+    if (start == finish) return 0;
+    return RecursiveNumber(start+1, finish);
+}
+
+Console.Write("Введите значение M: ");
+int m = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите значение N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+if (m > n) Console.WriteLine("M должно быть меньше N");
+else RecursiveNumber(m,n);
+
---
+++ task3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++) Console.Write($"{array[i]} ");
+    Console.WriteLine();
+}
+
+
+int[] FillInArray(int n)
+{
+    int[] array = new int [n];
+    for (int i = 0; i < n; i++) array[i] = new Random().Next(0,100);
+    Console.WriteLine("Оригинал:");
+    PrintArray(array);
+    return array;
+}
+
+
+void ReverseArray(int[] array, int i)
+{   
+    if (i < 0) return;
+    Console.Write($"{array[i]} ");    
+    ReverseArray(array, i-1);
+}
+
+Console.Write("Введите длину массива: ");
+int[] arrayfirst = FillInArray(int.Parse(Console.ReadLine()!));
+Console.WriteLine("Реверс:");
+ReverseArray(arrayfirst, arrayfirst.Length-1);
+
